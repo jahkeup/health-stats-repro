bfdde59877476f6aaccc1e355e3c14c2d48bbc45
Jacob Vallejo <you@jahkeup.com>
2018-03-21 11:46:59 -0700

    Add license

--- main.go
+++ main.go
@@ -1,4 +1,18 @@
-// @jahkeup | jakeev@amazon.com
+// Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 //
 // run the resulting build with:
 //
@@ -39,33 +53,6 @@ var (
 
 func init() {
 	progT = time.Now()
-}
-
-func buildImageOptions(name string) docker.BuildImageOptions {
-	log.Println("Building docker container for test")
-	t := time.Now()
-	inputbuf := bytes.NewBuffer(nil)
-	tr := tar.NewWriter(inputbuf)
-	data := []byte(dockerfile)
-	tr.WriteHeader(&tar.Header{Name: "Dockerfile", Size: int64(len(data)), ModTime: t, AccessTime: t, ChangeTime: t})
-	tr.Write(data)
-	tr.Close()
-	opts := docker.BuildImageOptions{
-		Name:         name,
-		InputStream:  inputbuf,
-		OutputStream: os.Stdout,
-	}
-	return opts
-}
-
-func createContainer(client *docker.Client) (*docker.Container, error) {
-	container, err := client.CreateContainer(docker.CreateContainerOptions{
-		Config: &docker.Config{
-			Image: imageName,
-		},
-	})
-
-	return container, err
 }
 
 func main() {
@@ -218,6 +205,33 @@ func logStatsForContainers(ctx context.Context, out io.Writer, client *docker.Cl
 		}
 	}
 
+}
+
+func buildImageOptions(name string) docker.BuildImageOptions {
+	log.Println("Building docker container for test")
+	t := time.Now()
+	inputbuf := bytes.NewBuffer(nil)
+	tr := tar.NewWriter(inputbuf)
+	data := []byte(dockerfile)
+	tr.WriteHeader(&tar.Header{Name: "Dockerfile", Size: int64(len(data)), ModTime: t, AccessTime: t, ChangeTime: t})
+	tr.Write(data)
+	tr.Close()
+	opts := docker.BuildImageOptions{
+		Name:         name,
+		InputStream:  inputbuf,
+		OutputStream: os.Stdout,
+	}
+	return opts
+}
+
+func createContainer(client *docker.Client) (*docker.Container, error) {
+	container, err := client.CreateContainer(docker.CreateContainerOptions{
+		Config: &docker.Config{
+			Image: imageName,
+		},
+	})
+
+	return container, err
 }
 
 func logEvents(ctx context.Context, out io.Writer, events <-chan *docker.APIEvents) {
